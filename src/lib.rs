40943755fb03ec89e60baed5fd31d565a0a314ff
arad <neginflus@gmail.com>
2023-03-21 00:22:58 +0330

    cleaned up code

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,44 @@
 // the addresses
-const PRINT_ADDY: u32 = 0x107ACC0;
+const PRINT_ADDRESS: u32 = 0x107ACC0;
 
 // the code that uses the address to print
-use std::{ffi::c_void, ptr::null_mut};
+use std::{ffi::c_void, ptr::null_mut}; 
 use core::mem::transmute;
-use winapi::{ctypes::c_char, shared::minwindef::{BOOL, HMODULE, DWORD, LPVOID}, um::{minwinbase::LPTHREAD_START_ROUTINE, winnt::{DLL_PROCESS_ATTACH, DLL_PROCESS_DETACH}, processthreadsapi::ExitProcess}};
+use winapi::{shared::minwindef::{BOOL, HMODULE, DWORD, LPVOID}, um::{winnt::{DLL_PROCESS_ATTACH, DLL_PROCESS_DETACH}}};
+use std::ffi::*;
+use winapi::um::*;
 
 unsafe  fn x(x: u32) -> *mut c_void {
-    (x - 0x400000 + (winapi::um::libloaderapi::GetModuleHandleA((0 as *const i8)) 
-    as winapi::shared::minwindef::DWORD))
-     as *mut c_void
+    (x - 0x400000 + (libloaderapi::GetModuleHandleA(0 as *const i8) as DWORD)) as *mut c_void
 }
 
-type r_print_t = extern fn(std::ffi::c_int, *const std::ffi::c_char) -> usize;
+type Rprint = extern fn(c_int, *const c_char) -> usize;
 
 
-extern "system" fn dllentry(r:LPVOID) -> DWORD {
-    let r_lua_print: r_print_t = unsafe {transmute(x(PRINT_ADDY)) };
-    r_lua_print(1, unsafe {transmute(b"this is a string\0")});
+extern "system" fn entry(r:LPVOID) -> DWORD {
+    let print: Rprint = unsafe {transmute(x(PRINT_ADDRESS)) };
+    print(1, unsafe {transmute(b"this is a string\0")});
+    print(1, unsafe {transmute(b"printsploit winning\0")});
     0
 }
 
 #[no_mangle]
 pub extern "system" fn DllMain( hModule:HMODULE, dw_reason:DWORD, lpReserved:LPVOID ) -> BOOL {
     if (dw_reason == DLL_PROCESS_ATTACH) {
-        unsafe {winapi::um::processthreadsapi::CreateThread(null_mut(), 
-            10000000,
-            Some(dllentry) , 
-            null_mut(),
-             0,
-              null_mut());}
+        unsafe 
+            {
+            processthreadsapi::CreateThread(
+                null_mut(), 
+                10000000,
+                Some(entry), 
+                null_mut(),
+                0,
+                null_mut()
+            );
+            }
     }
     if (dw_reason == DLL_PROCESS_DETACH) {
-
+                //will probs have to put shit here later
     }
     1
 }
