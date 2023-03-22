e35ca00bfd7f6d1b1d54c04e41ddf5c59b6b4e95
arad <neginflus@gmail.com>
2023-03-22 23:19:23 +0430

    updated

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 // the addresses
-const PRINT_ADDRESS: u32 = 0x107ACC0;
+const PRINT_ADDRESS: u32 = 0xCF8200;
 
 // the code that uses the address to print
 use std::{ffi::c_void, ptr::null_mut}; 
@@ -9,14 +9,14 @@ use std::ffi::*;
 use winapi::um::*;
 
 unsafe  fn x(x: u32) -> *mut c_void {
-    (x - 0x400000 + (libloaderapi::GetModuleHandleA(0 as *const i8) as DWORD)) as *mut c_void
+    (x - 0 + (libloaderapi::GetModuleHandleA(0 as *const i8) as DWORD)) as *mut c_void
 }
 
 type Rprint = extern fn(c_int, *const c_char) -> usize;
 
 
 extern "system" fn entry(r:LPVOID) -> DWORD {
-    let print: Rprint = unsafe {transmute(x(PRINT_ADDRESS)) };
+    let print: Rprint = unsafe {transmute(x(PRINT_ADDRESS))};
     print(1, unsafe {transmute(b"this is a string\0")});
     print(1, unsafe {transmute(b"printsploit winning\0")});
     0
