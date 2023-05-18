78b8beb85b25b9dff1f60e44eaa786b1436e05bb
arad <72018476+araddev@users.noreply.github.com>
2023-05-18 19:52:51 +0330

    Delete target/release directory

--- target/release/build/syn-f8c4e4566828b599/build_script_build-f8c4e4566828b599.d
+++
@@ -1,5 +0,0 @@
-D:\Users\Arad\Desktop\RustLearning\dll\target\release\build\syn-f8c4e4566828b599\build_script_build-f8c4e4566828b599.exe: C:\Users\Arad\.cargo\registry\src\github.com-1ecc6299db9ec823\syn-1.0.109\build.rs
-
-D:\Users\Arad\Desktop\RustLearning\dll\target\release\build\syn-f8c4e4566828b599\build_script_build-f8c4e4566828b599.d: C:\Users\Arad\.cargo\registry\src\github.com-1ecc6299db9ec823\syn-1.0.109\build.rs
-
-C:\Users\Arad\.cargo\registry\src\github.com-1ecc6299db9ec823\syn-1.0.109\build.rs:
--- target/release/rust_tutorial.d
+++
@@ -1,1 +0,0 @@
-D:\Users\Arad\Desktop\RustLearning\dll\target\release\rust_tutorial.dll: D:\Users\Arad\Desktop\RustLearning\dll\src\lib.rs
