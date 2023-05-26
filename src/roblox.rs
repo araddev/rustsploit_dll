2092923831addfbac0bd4c763cdfbed60f1fec67
arad <neginflus@gmail.com>
2023-05-26 18:52:50 +0330

    ignore overflow

--- src/roblox.rs
+++ src/roblox.rs
@@ -1,11 +1,12 @@
 use std::{ffi::{c_int, c_char}, time::Duration};
+use std::ffi::CString;
 
 pub fn x(address: usize) -> usize {
     let base = unsafe { winapi::um::libloaderapi::GetModuleHandleA(std::ptr::null()) } as usize;
     (address + base) - 0x400000
 }
 
-pub fn print(type_val: u8, content: &str) -> c_int {
+pub fn print(type_val: u8, content: CString) -> c_int {
     let address = x(0xEF9490);
     unsafe {
         let func_ptr: extern "C" fn(u8, *const c_char) -> i32 =
@@ -19,6 +20,13 @@ pub fn print_to_roblox(thingtoprint: &str, mut typeofprint: u8){
     if typeofprint>3 {
         typeofprint = 3;
     }
-    print(typeofprint, &format!("{}", thingtoprint));
+    print(typeofprint, convert_to_cstring(thingtoprint));
     std::thread::sleep(Duration::from_secs(1));
+}
+
+fn convert_to_cstring(input: &str) -> CString {
+    match CString::new(input) {
+        Ok(cstring) => cstring,
+        Err(_) => panic!("Failed to convert &str to CString"),
+    }
 }
